be61980210cd12a6e5498067006c66844e32bacc
fei <gogmianfei@gmail.com>
2018-03-19 23:04:15 +0800

    刚刚添加第二个界面并未完善功能

---
+++ app/src/main/java/com/syyk/mvpstudy/mvp/view/activity/NewsDetailActivity.java
@@ -0,0 +1,35 @@
+package com.syyk.mvpstudy.mvp.view.activity;
+
+import com.syyk.mvpstudy.BaseActivity;
+import com.syyk.mvpstudy.R;
+
+/**
+ * Created by Administrator on 2018/3/19 0019.
+ */
+
+public class NewsDetailActivity extends BaseActivity {
+    @Override
+    public int setLayout() {
+        return R.layout.activity_newdetail;
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void setupComponent() {
+
+    }
+}
